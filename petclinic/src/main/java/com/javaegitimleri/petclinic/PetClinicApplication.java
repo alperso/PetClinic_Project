882057b4cbee95ed1074fb7231cc79341f08c401
alper.oner <alper.oner@krontech.com>
2024-11-29 22:10:46 +0300

    1-actuator varsayilan endpointlerin disinda endpointlerin eklenmesi

---
+++ petclinic/src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
@@ -0,0 +1,15 @@
+package com.javaegitimleri.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(PetClinicApplication.class, args);
+
+	}
+
+}
