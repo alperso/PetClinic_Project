d4f8afaca4aa423ad914d56aec461f6bdd737dd1
alper.oner <alper.oner@krontech.com>
2024-12-06 19:28:33 +0300

    4-PetClinic Projesi Katmanli Mimari @Service Anotasyonu unutulmus, sonradan eklendi.

--- petclinic/src/main/java/com/javaegitimleri/petclinic/service/PetClinicServiceImpl.java
+++ petclinic/src/main/java/com/javaegitimleri/petclinic/service/PetClinicServiceImpl.java
@@ -3,13 +3,26 @@ package com.javaegitimleri.petclinic.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.javaegitimleri.petclinic.dao.OwnerRepository;
 import com.javaegitimleri.petclinic.exception.OwnerNotFoundException;
 import com.javaegitimleri.petclinic.model.Owner;
 
+
+/*
+ * @Service anotasyonu, Spring Framework'te bir sınıfı servis katmanı olarak işaretler.
+ * Bu anotasyon, sınıfın iş mantığı taşıyan bir bean olduğunu belirtir ve Spring'in bu sınıfı otomatik olarak
+ * Spring konteynerine eklemesini sağlar.
+ * @Service ile işaretlenmiş sınıflar,
+ * genellikle veritabanı işlemleri veya iş mantığı gibi uygulama işlevlerini yerine getirir.
+ *
+ * @Service, @Component anotasyonunun özel bir türüdür ve yalnızca anlam olarak iş mantığı katmanı olduğunu belirtir.
+ *
+ * */
+@Service
 public class PetClinicServiceImpl implements PetClinicService {
-	
+
 	@Autowired
 	private OwnerRepository ownerRepository;
 
@@ -28,7 +41,7 @@ public class PetClinicServiceImpl implements PetClinicService {
 		Owner owner = ownerRepository.findById(id);
 		if (owner==null) throw new OwnerNotFoundException("Owner Bulunamadı");
 		return owner;
-		
+
 	}
 
 	@Override
