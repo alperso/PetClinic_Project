28f9214eeb0a518231212d0838407afb10e75204
alper.oner <alpersahinoner@gmail.com>
2024-12-08 14:20:24 +0300

    16-Response Content Tipinin(.xml,.json) olarak Owner Objesinde belirlenmesi(@XmlRootElement,@XmlTransient,@JsonIgnore kullanimi)

--- petclinic/src/main/java/com/javaegitimleri/petclinic/model/Owner.java
+++ petclinic/src/main/java/com/javaegitimleri/petclinic/model/Owner.java
@@ -1,8 +1,29 @@
 package com.javaegitimleri.petclinic.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.HashSet;
 import java.util.Set;
 
+//Owner kayıtlarının xml e donusturulmesi icin Jaxb - XMLRootElement Anotasyonunun aktif olması gerekir
+
+//@XmlRootElement: Sınıfın XML'de bir kök eleman olarak kullanılmasını sağlar.
+//<owner>
+//	<id>1</id>
+//	<firstName>John</firstName>
+//	<lastName>Doe</lastName>
+//</owner>
+
+//@XmlTransient: Belirli bir alanın XML çıktısında görünmesini engeller.
+//@JsonIgnore: Belirli bir alanın JSON çıktısında görünmesini engeller.
+
+//Eğer Accept: application/json başlığı varsa → JSON döner.
+//Eğer Accept: application/xml başlığı varsa → XML döner.
+//Hiçbir şey belirtilmezse → Varsayılan (genelde JSON) döner.
+
+@XmlRootElement
 public class Owner {
 	
 	private Long id;
@@ -29,6 +50,10 @@ public class Owner {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	//XML ve JSON Response a bu kısmı alma demek(donus kısmında bu gozukmeyecek)
+	@XmlTransient
+	@JsonIgnore
 	public Set<Pet> getPets() {
 		return pets;
 	}
